c076045090f041e349d20aaddb736e15138c9d05
itsaniketdubey <aniket.dubey74@nmims.edu.in>
2021-10-01 19:46:11 +0530

    Added Custom User Model

--- wpproj/settings.py
+++ wpproj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #user apps
+    # user apps
     'login.apps.LoginConfig',
     'dashboard.apps.DashboardConfig',
 ]
@@ -83,7 +83,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'dashboard.User'
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
