34770e9e423384cf590a537185107903a3f1496e
Aryan Reddy <aryan61101@gmail.com>
2021-10-07 11:00:13 +0530

    Added Login Required

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
 
+@login_required(login_url='login')
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
--- login/views.py
+++ login/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'login/login.html')
 
+@login_required(login_url='login')
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
