0d1dd7cbd88964589d44d3d6c815626c8d24ce93
itsaniketdubey <aniket.dubey74@nmims.edu.in>
2021-10-12 14:27:21 +0530

    urls.py: Login now uses LoginView instead of Forms

--- login/urls.py
+++ login/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 from login.forms import UserLoginForm
 
 urlpatterns = [
-    path('',views.LoginView.as_view(
-                template_name = "login/login.html",
-                authentication_form = UserLoginForm
-            ), name='login'
-    ),
+    path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
 ]
 
 
