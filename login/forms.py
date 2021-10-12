900a6606fb1fa81ce92907790025be554c773139
itsaniketdubey <aniket.dubey74@nmims.edu.in>
2021-10-12 15:26:14 +0530

    Modified Login

--- login/forms.py
+++ login/forms.py
@@ -4,13 +4,13 @@ from django import forms
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'SAP'})
 
     username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'E-Mail ID', 'id': 'hello'}))
+        attrs={'class': 'form-control', 'label':'sap','placeholder': 'SAP ID'}))
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={
             'class': 'form-control',
             'placeholder': 'Password',
-            'id': 'hi',
         }
 ))
--- login/urls.py
+++ login/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from login.forms import UserLoginForm
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('',auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
 ]
 
 
